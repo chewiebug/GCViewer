8c71f391b6134a5a1ab6b50fe8ea56ad1901e476
chewiebug <jwu@gmx.ch>
2012-01-30 21:35:21 +0100

    refactor parsing of G1 logs

--- src/main/java/com/tagtraum/perf/gcviewer/imp/DataReaderSun1_6_0G1.java
+++ src/main/java/com/tagtraum/perf/gcviewer/imp/DataReaderSun1_6_0G1.java
@@ -1,5 +1,6 @@
 package com.tagtraum.perf.gcviewer.imp;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
@@ -45,6 +46,13 @@ public class DataReaderSun1_6_0G1 extends AbstractDataReaderSun {
 
     private static final String TIMES = "[Times";
     
+    private static final String TIMES_ALONE = " " + TIMES;
+    private static final List<String> EXCLUDE_STRINGS = new LinkedList<String>();
+
+    static {
+        EXCLUDE_STRINGS.add(TIMES_ALONE);
+    }
+    
     // the following pattern is specific for G1 with -XX:+PrintGCDetails
     // "0.295: [GC pause (young), 0.00594747 secs]"
     private static final Pattern PATTERN_GC_PAUSE = Pattern.compile("^([0-9.]+)[: \\[]{3}([A-Za-z- ()]+)[, ]+([0-9.]+)[ sec\\]]+$");
@@ -85,7 +93,7 @@ public class DataReaderSun1_6_0G1 extends AbstractDataReaderSun {
 
     @Override
     public GCModel read() throws IOException {
-        if (LOG.isLoggable(Level.INFO)) LOG.info("Reading Sun 1.6.x G1 format...");
+        if (LOG.isLoggable(Level.INFO)) LOG.info("Reading Sun 1.6.x / 1.7.x G1 format...");
 
         try {
             final GCModel model = new GCModel(true);
@@ -93,104 +101,85 @@ public class DataReaderSun1_6_0G1 extends AbstractDataReaderSun {
             model.setFormat(GCModel.Format.SUN_X_LOG_GC);
             String line;
             final ParsePosition parsePosition = new ParsePosition(0);
-            boolean isInDetailedEvent = false;
             Matcher gcPauseMatcher = PATTERN_GC_PAUSE.matcher("");
-            Matcher memoryMatcher = PATTERN_MEMORY.matcher("");
             Matcher linesMixedMatcher = PATTERN_LINES_MIXED.matcher("");
             GCEvent gcEvent = null;
             int lineNumber = 0;
             String beginningOfLine = null;
+            OUTERLOOP:
             while ((line = in.readLine()) != null) {
                 ++lineNumber;
                 parsePosition.setLineNumber(lineNumber);
+                parsePosition.setIndex(0);
                 if ("".equals(line)) {
                     continue;
                 }
                 try {
-                    if (!isInDetailedEvent) {
-                        // if a new timestamp occurs in the middle of a line, that should be treated as a new line
-                        // -> the rest of the old line appears on the next line
-                        linesMixedMatcher.reset(line); 
-                        if (linesMixedMatcher.matches()) {
-                            if (linesMixedMatcher.group(3) == null && linesMixedMatcher.group(5) == null) {
-                                // 1st pattern (complete concurrent collection follows)
-                                beginningOfLine = linesMixedMatcher.group(1);
-                                model.add(parseLine(linesMixedMatcher.group(2), parsePosition));
-                                parsePosition.setIndex(0);
-                                continue; // rest of collection is on the next line, so continue there
-                            }
-                            else if (linesMixedMatcher.group(5) == null) {
-                                // 2nd pattern; only timestamp is part of concurrent event
-                                beginningOfLine = linesMixedMatcher.group(3); // only timestamp
-                                line = linesMixedMatcher.group(1) + linesMixedMatcher.group(4);
-                            }
-                            else if (linesMixedMatcher.group(3) == null) {
-                                // 3rd pattern; again only timestamp is part of concurrent event
-                                beginningOfLine = linesMixedMatcher.group(5); // only timestamp
-                                line = linesMixedMatcher.group(1) + linesMixedMatcher.group(6);
-                            }
+                    // filter out lines that don't need to be parsed
+                    for (String i : EXCLUDE_STRINGS) {
+                        if (line.indexOf(i) == 0) continue OUTERLOOP;
+                    }
+                    // if a new timestamp occurs in the middle of a line, that should be treated as a new line
+                    // -> the rest of the old line appears on the next line
+                    linesMixedMatcher.reset(line); 
+                    if (linesMixedMatcher.matches()) {
+                        if (linesMixedMatcher.group(3) == null && linesMixedMatcher.group(5) == null) {
+                            // 1st pattern (complete concurrent collection follows)
+                            beginningOfLine = linesMixedMatcher.group(1);
+                            model.add(parseLine(linesMixedMatcher.group(2), parsePosition));
+                            parsePosition.setIndex(0);
+                            continue; // rest of collection is on the next line, so continue there
                         }
-                        else if (beginningOfLine != null) {
-                            // not detailed log but mixed line
-                            line = beginningOfLine + line;
-                            beginningOfLine = null;
+                        else if (linesMixedMatcher.group(5) == null) {
+                            // 2nd pattern; only timestamp is part of concurrent event
+                            beginningOfLine = linesMixedMatcher.group(3); // only timestamp
+                            line = linesMixedMatcher.group(1) + linesMixedMatcher.group(4);
                         }
-
-                        // the following case is special for -XX:+PrintGCDetails and must be treated
-                        // different from the other cases occuring in G1 standard mode
-                        // 0.356: [GC pause (young), 0.00219944 secs] -> GC_PAUSE pattern but GC_MEMORY_PAUSE 
-                        //   event (has extensive details)
-                        // all other GC types are the same as in standard G1 mode.
-                        gcPauseMatcher.reset(line);
-                        if (gcPauseMatcher.matches()) {
-                            AbstractGCEvent.Type type = AbstractGCEvent.Type.parse(gcPauseMatcher.group(GC_TYPE));
-                            if (type != null && type.getPattern().compareTo(GcPattern.GC_MEMORY_PAUSE) == 0) {
-                                // detailed G1 events start with GC_MEMORY pattern, but are of type GC_MEMORY_PAUSE
-                                isInDetailedEvent = true;
-    
-                                gcEvent = new G1GcEvent();
-                                gcEvent.setTimestamp(Double.parseDouble(gcPauseMatcher.group(GC_TIMESTAMP)));
-                                gcEvent.setType(type);
-                                gcEvent.setPause(Double.parseDouble(gcPauseMatcher.group(GC_PAUSE)));
-                            }
-                            else {
-                                model.add(parseLine(line, parsePosition));
-                            }
-                        }
-                        else if (line.indexOf(HEAP_SIZING_START) >= 0) {
-                            // the next few lines will be the sizing of the heap
-                            lineNumber = skipLines(in, parsePosition, lineNumber, HEAP_STRINGS);
-                            continue;
-                        }
-                        else if (line.indexOf(TIMES) < 0) {
-                            model.add(parseLine(line, parsePosition));
+                        else if (linesMixedMatcher.group(3) == null) {
+                            // 3rd pattern; again only timestamp is part of concurrent event
+                            beginningOfLine = linesMixedMatcher.group(5); // only timestamp
+                            line = linesMixedMatcher.group(1) + linesMixedMatcher.group(6);
                         }
                     }
                     else if (beginningOfLine != null) {
-                        // detailed log and ugly mixed line -> 2nd part 
-                        // (only timestamp of concurrent collection in 1st line; 
-                        // rest of concurrent collection in 2nd line)
+                        // not detailed log but mixed line
                         line = beginningOfLine + line;
                         beginningOfLine = null;
-                        model.add(parseLine(line, parsePosition));
+                    }
+
+                    // the following case is special for -XX:+PrintGCDetails and must be treated
+                    // different from the other cases occuring in G1 standard mode
+                    // 0.356: [GC pause (young), 0.00219944 secs] -> GC_PAUSE pattern but GC_MEMORY_PAUSE 
+                    //   event (has extensive details)
+                    // all other GC types are the same as in standard G1 mode.
+                    gcPauseMatcher.reset(line);
+                    if (gcPauseMatcher.matches()) {
+                        AbstractGCEvent.Type type = AbstractGCEvent.Type.parse(gcPauseMatcher.group(GC_TYPE));
+                        if (type != null && type.getPattern().compareTo(GcPattern.GC_MEMORY_PAUSE) == 0) {
+                            // detailed G1 events start with GC_MEMORY pattern, but are of type GC_MEMORY_PAUSE
+
+                            gcEvent = new G1GcEvent();
+                            gcEvent.setTimestamp(Double.parseDouble(gcPauseMatcher.group(GC_TIMESTAMP)));
+                            gcEvent.setType(type);
+                            gcEvent.setPause(Double.parseDouble(gcPauseMatcher.group(GC_PAUSE)));
+                            
+                            // now parse the details of this event
+                            lineNumber = parseDetails(in, model, parsePosition, lineNumber, gcEvent, beginningOfLine);
+                            beginningOfLine = null;
+                            continue;
+                        }
+                        else {
+                            // real GC_PAUSE events like some concurrent events
+                            model.add(parseLine(line, parsePosition));
+                        }
+                    }
+                    else if (line.indexOf(HEAP_SIZING_START) >= 0) {
+                        // the next few lines will be the sizing of the heap
+                        lineNumber = skipLines(in, parsePosition, lineNumber, HEAP_STRINGS);
+                        continue;
                     }
                     else {
-                        // now we parse details of a pause
-                        // currently everything except memory is skipped
-                        memoryMatcher.reset(line);
-                        if (memoryMatcher.matches()) {
-                            setMemory(gcEvent, line, parsePosition);
-                        }
-                        
-                    }
-                    if (line.indexOf(TIMES) >= 0) {
-                        // detailed gc description ends with " [Times: user=...]" -> finalize gcEvent
-                        // otherwise just ignore 
-                        if (gcEvent != null) {
-                            model.add(gcEvent);
-                            gcEvent = null;
-                            isInDetailedEvent = false;
-                        }
+                        model.add(parseLine(line, parsePosition));
                     }
                 } catch (Exception pe) {
                     if (LOG.isLoggable(Level.WARNING)) LOG.log(Level.WARNING, pe.getMessage());
@@ -210,6 +199,66 @@ public class DataReaderSun1_6_0G1 extends AbstractDataReaderSun {
         }
     }
 
+    /**
+     * Parses details of a standard gc collection event (e.g. GC pause (young)) with
+     * -XX:+PrintGCDetails
+     * 
+     * @param in reader reading from log file
+     * @param model current model
+     * @param pos parsePosition
+     * @param lineNumber line number of last line read
+     * @param event current event
+     * @param beginningOfLine GC_PAUSE lines are sometimes mixed lines; the extracted parts
+     * of such a line are stored inside "beginningOfLine"
+     * @return line number of last line read in this method
+     * @throws ParseException Problem parsing a part of the details
+     * @throws IOException problem reading from file
+     */
+    private int parseDetails(BufferedReader in, 
+            GCModel model,
+            ParsePosition pos, 
+            int lineNumber, 
+            GCEvent event, 
+            String beginningOfLine)
+                    throws ParseException, IOException {
+        
+        Matcher memoryMatcher = PATTERN_MEMORY.matcher("");
+
+        pos.setIndex(0);
+        boolean isInDetailedEvent = true;
+        String line;
+        while (isInDetailedEvent && (line = in.readLine()) != null) {
+            ++lineNumber;
+            pos.setLineNumber(lineNumber);
+            
+            // we might have had a mixed line before; then we just parsed the second part of the mixed line
+            if (beginningOfLine != null) {
+                line = beginningOfLine + line;
+                beginningOfLine = null;
+                model.add(parseLine(line, pos));
+                pos.setIndex(0);
+                continue;
+            }
+            
+            // now we parse details of a pause
+            // currently everything except memory is skipped
+            memoryMatcher.reset(line);
+            if (memoryMatcher.matches()) {
+                setMemory(event, line, pos);
+                pos.setIndex(0);
+            }
+
+            if (line.indexOf(TIMES) >= 0) {
+                // detailed gc description ends with " [Times: user=...]" -> stop reading lines
+                isInDetailedEvent = false;
+            }
+        }
+        
+        model.add(event);
+
+        return lineNumber;
+    }
+    
     @Override
     protected AbstractGCEvent parseLine(final String line, final ParsePosition pos) throws ParseException {
         AbstractGCEvent ae = null;
@@ -236,9 +285,12 @@ public class DataReaderSun1_6_0G1 extends AbstractDataReaderSun {
                 final GCEvent event = new GCEvent();
                 event.setTimestamp(timestamp);
                 event.setType(type);
+                // Java 7 can have detailed event at this position like this
+                // 0.197: [GC remark 0.197: [GC ref-proc, 0.0000070 secs], 0.0005297 secs]
                 if (isTimestamp(line, pos)) {
                     event.add(parseLine(line, pos));
                 }
+                
                 if (event.getType().getPattern() == GcPattern.GC_MEMORY_PAUSE) {
                     setMemoryAndPauses((GCEvent)event, line, pos);
                 }
