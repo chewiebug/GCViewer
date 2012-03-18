144da17b676b9ea2a28b63db46848f094050d7e6
chewiebug <jwu@gmx.ch>
2012-03-18 21:54:50 +0100

    fix NullPointerException when unexpected NewLines occur

--- src/main/java/com/tagtraum/perf/gcviewer/imp/DataReaderSun1_6_0.java
+++ src/main/java/com/tagtraum/perf/gcviewer/imp/DataReaderSun1_6_0.java
@@ -72,7 +72,6 @@ public class DataReaderSun1_6_0 extends AbstractDataReaderSun {
     }
     
     private static final String EVENT_YG_OCCUPANCY = "YG occupancy";
-    // TODO is combination with DefNew also possible?
     private static final String EVENT_PARNEW = "ParNew";
     private static final String EVENT_DEFNEW = "DefNew";
     
@@ -119,7 +118,7 @@ public class DataReaderSun1_6_0 extends AbstractDataReaderSun {
     // pattern looks always like "...[CMS<datestamp>..." or "...[CMS<timestamp>..."
     // the next line starts with " (concurrent mode failure)" which in earlier releases followed "CMS" immediately
     // the same can happen with "...ParNew<timestamp|datestamp>..."
-    private static Pattern linesMixedPattern = Pattern.compile("(.* \\[(CMS|ParNew))([0-9]+[-.].*)");
+    private static Pattern linesMixedPattern = Pattern.compile("(.* \\[(CMS|ParNew|DefNew))([0-9]+[-.].*)");
     // Matcher group of start of line
     private static final int LINES_MIXED_STARTOFLINE_GROUP = 1;
     // Matcher group of end of line
@@ -234,10 +233,10 @@ public class DataReaderSun1_6_0 extends AbstractDataReaderSun {
                         continue;
                     }
                     model.add(parseLine(line, parsePosition));
-                } catch (ParseException pe) {
+                } catch (Exception pe) {
                     if (LOG.isLoggable(Level.WARNING)) LOG.warning(pe.toString());
                     if (LOG.isLoggable(Level.FINE)) LOG.log(Level.FINE, pe.getMessage(), pe);
-                    beginningOfLine = null;
+                    beginningOfLine.clear();
                 }
                 // reset ParsePosition
                 parsePosition.setIndex(0);
