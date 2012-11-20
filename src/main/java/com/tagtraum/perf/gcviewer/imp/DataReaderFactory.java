e696fb88be4eefdf1491e60ad41121097e16ba3c
chewiebug <jwu@gmx.ch>
2012-11-20 22:18:53 +0100

    better recognition of JRockit1.5.0

--- src/main/java/com/tagtraum/perf/gcviewer/imp/DataReaderFactory.java
+++ src/main/java/com/tagtraum/perf/gcviewer/imp/DataReaderFactory.java
@@ -73,7 +73,7 @@ public class DataReaderFactory {
 
     private DataReader getDataReaderBySample(String s, InputStream in) throws IOException {
         // if there is a [memory ] somewhere in the first chunk of the logs, it is JRockit
-        if (s.indexOf("[memory ] ") != -1) {
+        if (s.indexOf("[memory ]") != -1) {
             // JRockit 1.5 and 1.6 logs look like: [memory ][Tue Nov 13 08:39:01 2012][01684] [OC#1]
             if ((s.indexOf("[YC#") != -1) ||(s.indexOf("[OC#") != -1)) {
                 if (LOG.isLoggable(Level.INFO)) LOG.info("File format: JRockit 1.6");
