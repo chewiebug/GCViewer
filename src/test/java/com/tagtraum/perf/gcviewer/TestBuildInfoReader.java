5723fe35aa2790f7810fb0b3b975af43b63f8f95
chewiebug <jwu@gmx.ch>
2014-10-15 15:45:44 +0200

    read build information from MANIFEST file

--- src/test/java/com/tagtraum/perf/gcviewer/TestBuildInfoReader.java
+++ src/test/java/com/tagtraum/perf/gcviewer/TestBuildInfoReader.java
@@ -1,7 +1,9 @@
 package com.tagtraum.perf.gcviewer;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.Matchers.isEmptyOrNullString;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
@@ -10,7 +12,7 @@ import com.tagtraum.perf.gcviewer.util.BuildInfoReader;
 /**
  * Tests the class {@link BuildInfoReader} - makes sure that the properties from the file
  * can be read. 
- * 
+ *
  * @author <a href="mailto:gcviewer@gmx.ch">Joerg Wuethrich</a>
  * <p>created on: 05.12.2012</p>
  */
@@ -19,14 +21,14 @@ public class TestBuildInfoReader {
     @Test
     public void readVersion() {
         String version = BuildInfoReader.getVersion();
-        assertNotNull("version", version);
-        assertFalse("must not be n/a", version.equals("n/a"));
+        assertThat("version", version, notNullValue());
+        assertThat("must not be empty", version, not(isEmptyOrNullString()));
     }
 
     @Test
     public void readBuildDate() {
         String buildDate = BuildInfoReader.getBuildDate();
-        assertNotNull("buildDate", buildDate);
-        assertFalse("must not be n/a", buildDate.equals("n/a"));
+        assertThat("buildDate", buildDate, notNullValue());
+        assertThat("must not be empty", buildDate, not(isEmptyOrNullString()));
     }
 }
