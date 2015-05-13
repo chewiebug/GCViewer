72f0c8a5bbe69dc57eefe3c18c730255879ae0dc
chewiebug <jwu@gmx.ch>
2015-05-13 21:48:26 +0200

    reduce number of WARNING messages when parsing introduction of a detailed G1 message fails (#128)
    filter detail lines, that are not recognised as being part of a G1 detailed event information
    e.g.
       [Parallel Time: 22.1 ms, GC Workers: 10]
          [GC Worker Start (ms): Min: 5147.0, Avg: 5147.4, Max: 5147.8, Diff: 0.8]
          [Ext Root Scanning (ms): Min: 3.8, Avg: 4.5, Max: 5.2, Diff: 1.4, Sum: 44.7]

--- src/test/java/com/tagtraum/perf/gcviewer/imp/TestDataReaderSun1_8_0G1.java
+++ src/test/java/com/tagtraum/perf/gcviewer/imp/TestDataReaderSun1_8_0G1.java
@@ -16,8 +16,8 @@ import com.tagtraum.perf.gcviewer.model.GCEvent;
 import com.tagtraum.perf.gcviewer.model.GCModel;
 
 /**
- * Test logs generated specifically by JDK 1.8 G1 algorithm. 
- * 
+ * Test logs generated specifically by JDK 1.8 G1 algorithm.
+ *
  * @author <a href="mailto:gcviewer@gmx.ch">Joerg Wuethrich</a>
  * <p>created on: 22.07.2014</p>
  */
@@ -41,17 +41,17 @@ public class TestDataReaderSun1_8_0G1 {
 
         DataReader reader = getDataReader("SampleSun1_8_0G1_ConcurrentCycle.txt");
         GCModel model = reader.read();
-        
+
         assertThat("size", model.size(), is(10));
-        
+
         assertThat("tenured size after concurrent cycle", model.getPostConcurrentCycleTenuredUsedSizes().getMax(), is(31949 - 10*1024 - 3072));
         assertThat("heap size after concurrent cycle", model.getPostConcurrentCycleHeapUsedSizes().getMax(), is(31949));
-        
+
         assertThat("initiatingOccupancyFraction", model.getCmsInitiatingOccupancyFraction().getMax(), closeTo(0.69, 0.001));
-        
+
         assertThat("number of errors", handler.getCount(), is(0));
     }
-    
+
     /**
      * In java 8, suddenly the full gc events in G1 got detailed information about the generation
      * sizes again. Test, that they are parsed correctly.
@@ -68,7 +68,7 @@ public class TestDataReaderSun1_8_0G1 {
                         + "\n  [Eden: 4096.0K(532.0M)->0.0B(612.0M) Survivors: 80.0M->0.0B Heap: 5811.9M(12.0G)->3097.8M(12.0G)], [Metaspace: 95902K->95450K(1140736K)]"
                         + "\n [Times: user=12.34 sys=0.22, real=8.99 secs]")
                         .getBytes());
-        
+
         DataReader reader = new DataReaderSun1_6_0G1(in, GcLogType.SUN1_8);
         GCModel model = reader.read();
 
@@ -77,9 +77,9 @@ public class TestDataReaderSun1_8_0G1 {
         assertThat("yound before", event.getYoung().getPreUsed(), is(4096 + 80*1024));
         assertThat("tenured", event.getTenured().getTotal(), is(12*1024*1024 - 612*1024));
         assertThat("metaspace", event.getPerm().getTotal(), is(1140736));
-        
+
         assertThat("perm", model.getPermAllocatedSizes().getN(), is(1));
-        
+
         assertThat("warning count", handler.getCount(), is(0));
     }
 
@@ -89,29 +89,41 @@ public class TestDataReaderSun1_8_0G1 {
         handler.setLevel(Level.WARNING);
         IMP_LOGGER.addHandler(handler);
         DATA_READER_FACTORY_LOGGER.addHandler(handler);
-        
+
         DataReader reader = getDataReader("SampleSun1_8_0G1PrintGCCausePrintTenuringDistribution.txt");
         GCModel model = reader.read();
-        
+
         assertEquals("gc pause sum", 16.7578613, model.getPause().getSum(), 0.000000001);
-        
+
         assertEquals("number of errors", 0, handler.getCount());
     }
-    
+
     @Test
     public void printHeapAtGC() throws Exception {
         TestLogHandler handler = new TestLogHandler();
         handler.setLevel(Level.WARNING);
         IMP_LOGGER.addHandler(handler);
         DATA_READER_FACTORY_LOGGER.addHandler(handler);
-        
+
         DataReader reader = getDataReader("SampleSun1_8_0G1PrintHeapAtGc.txt");
         GCModel model = reader.read();
-        
+
         assertEquals("gc pause sum", 0.0055924, model.getPause().getSum(), 0.000000001);
-        
+
         assertEquals("number of errors", 0, handler.getCount());
     }
-    
-    
+
+    @Test
+    public void humongousMixed() throws Exception {
+        TestLogHandler handler = new TestLogHandler();
+        handler.setLevel(Level.WARNING);
+        IMP_LOGGER.addHandler(handler);
+        DATA_READER_FACTORY_LOGGER.addHandler(handler);
+
+        DataReader reader = getDataReader("SampleSun1_8_0G1HumongousMixed.txt");
+        GCModel model = reader.read();
+
+        assertThat("number of events", model.size(), is(1));
+        assertThat("number of errors", handler.getCount(), is(2));
+    }
 }
