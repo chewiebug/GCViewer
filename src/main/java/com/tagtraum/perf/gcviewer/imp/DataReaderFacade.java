f09bd590484efa5ba6f7f3fd60422a8f655d1cc5
Pierre Viret <pierre.viret@gmail.com>
2016-02-09 11:12:17 +0100

    Fix a problem with quotes for language=fr
    The quotes of the localized messages were not displayed and unit test "LocalisationHelperTest" crashed because the argument were not replaced.
    - DataReaderFacade.java: use the LocalisationHelper.getString with arguments
    instead of performing the format ourself.
    - localStrings_fr.properties: replace each single quote with two single 
    quotes as stated in the javadoc of MessageFormat class:
    https://docs.oracle.com/javase/8/docs/api/java/text/MessageFormat.html#patterns
    Otherwise the single quote won't appear in the message and the arguments are not parsed.

--- src/main/java/com/tagtraum/perf/gcviewer/imp/DataReaderFacade.java
+++ src/main/java/com/tagtraum/perf/gcviewer/imp/DataReaderFacade.java
@@ -138,7 +138,7 @@ public class DataReaderFacade {
      */
     private void showErrorDialog(final URL url, TextAreaLogHandler textAreaLogHandler, final Component parent) {
         final JPanel panel = new JPanel(new BorderLayout());
-        final JLabel messageLabel = new JLabel(new MessageFormat(LocalisationHelper.getString("datareader_parseerror_dialog_message")).format(new Object[]{textAreaLogHandler.getErrorCount(), url}));
+        final JLabel messageLabel = new JLabel(LocalisationHelper.getString("datareader_parseerror_dialog_message", textAreaLogHandler.getErrorCount(), url));
         messageLabel.setBorder(BorderFactory.createEmptyBorder(5, 0, 5, 0));
         panel.add(messageLabel, BorderLayout.NORTH);
         final JScrollPane textAreaScrollPane = new JScrollPane(textAreaLogHandler.getTextArea());
@@ -146,7 +146,7 @@ public class DataReaderFacade {
         panel.add(textAreaScrollPane, BorderLayout.CENTER);
         SwingUtilities.invokeLater(new Runnable(){
             public void run() {
-                JOptionPane.showMessageDialog(parent, panel, new MessageFormat(LocalisationHelper.getString("datareader_parseerror_dialog_title")).format(new Object[]{url}), JOptionPane.ERROR_MESSAGE);
+                JOptionPane.showMessageDialog(parent, panel, LocalisationHelper.getString("datareader_parseerror_dialog_title", url), JOptionPane.ERROR_MESSAGE);
             }
         });
     }
