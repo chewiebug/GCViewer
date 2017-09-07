7bf399d70868807363c4d58a63d34dd92282de63
chewiebug <jwu@gmx.ch>
2017-09-07 12:47:52 +0200

    #192 rewrite unittest to support parallel execution

--- src/test/java/com/tagtraum/perf/gcviewer/GCViewerTest.java
+++ src/test/java/com/tagtraum/perf/gcviewer/GCViewerTest.java
@@ -4,43 +4,21 @@ import com.tagtraum.perf.gcviewer.ctrl.impl.GCViewerGuiController;
 import com.tagtraum.perf.gcviewer.model.GCResource;
 import com.tagtraum.perf.gcviewer.model.GcResourceFile;
 import com.tagtraum.perf.gcviewer.model.GcResourceSeries;
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
 import java.util.Arrays;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.containsString;
-import static org.hamcrest.Matchers.isEmptyString;
+import static org.hamcrest.Matchers.is;
 import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.verify;
 
 /**
  * @author martin.geldmacher
  */
 public class GCViewerTest {
-
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
-    private PrintStream oldOut;
-    private PrintStream oldErr;
-
-    @Before
-    public void replaceStreams() {
-        oldOut = System.out;
-        oldErr = System.err;
-        System.setOut(new PrintStream(outContent));
-        System.setErr(new PrintStream(errContent));
-    }
-
-    @After
-    public void revertToOriginalStreams() {
-        System.setOut(oldOut);
-        System.setErr(oldErr);
-    }
 
     @Test
     public void singleArgumentOpensGui() throws Exception {
@@ -48,10 +26,9 @@ public class GCViewerTest {
         GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
 
         String[] args = {"some_gc.log"};
-        gcViewer.doMain(args);
+        int exitValue = gcViewer.doMain(args);
         verify(controller).startGui(new GcResourceFile("some_gc.log"));
-        assertThat(outContent.toString(), isEmptyString());
-        assertThat(errContent.toString(), isEmptyString());
+        assertThat("exitValue of doMain", exitValue, is(0));
     }
 
     @Test
@@ -60,10 +37,9 @@ public class GCViewerTest {
         GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
 
         String[] args = {"some_gc.log.0;some_gc.log.1;some_gc.log.2"};
-        gcViewer.doMain(args);
+        int exitValue = gcViewer.doMain(args);
         verify(controller).startGui(new GcResourceSeries(Arrays.asList(new GcResourceFile("some_gc.log.0"), new GcResourceFile("some_gc.log.1"), new GcResourceFile("some_gc.log.2"))));
-        assertThat(outContent.toString(), isEmptyString());
-        assertThat(errContent.toString(), isEmptyString());
+        assertThat("result of doMain", exitValue, is(0));
     }
 
     @Test
@@ -72,9 +48,8 @@ public class GCViewerTest {
         GCViewer gcViewer = new GCViewer(controller, new GCViewerArgsParser());
 
         String[] args = {"argument1", "argument2", "argument3", "argument4"};
-        gcViewer.doMain(args);
+        int exitValue = gcViewer.doMain(args);
         verify(controller, never()).startGui(any(GCResource.class));
-        assertThat(outContent.toString(), containsString("Welcome to GCViewer with cmdline"));
-        assertThat(errContent.toString(), isEmptyString());
+        assertThat("result of doMain", exitValue, is(-3));
     }
 }
