d04f11d1eed6f307b3add4b3ee5c0432c3f3f36d
chewiebug <jwu@gmx.ch>
2020-04-29 16:02:18 +0200

    add jdk14 to test matrix

--- src/test/java/com/tagtraum/perf/gcviewer/imp/TestDataReaderFacade.java
+++ src/test/java/com/tagtraum/perf/gcviewer/imp/TestDataReaderFacade.java
@@ -89,8 +89,14 @@ public class TestDataReaderFacade {
         }
         catch (DataReaderException e) {
             assertNotNull("cause", e.getCause());
+            Class expectedClass;
+            if (System.getProperty("java.version").startsWith("14")) {
+                expectedClass = IOException.class;
+            } else {
+                expectedClass = IllegalArgumentException.class;
+            }
             assertEquals("expected exception in cause",
-                    IllegalArgumentException.class.getName(),
+                    expectedClass.getName(),
                     e.getCause().getClass().getName());
         }
     }
